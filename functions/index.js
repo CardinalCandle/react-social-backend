42e6b8b36114141deedbb0fae0e634a376216496
CardinalCandle <youssef-kj@outlook.fr>
2023-08-28 23:23:17 +0100

    modified signup function

--- functions/index.js
+++ functions/index.js
@@ -90,6 +90,7 @@ app.post('/signup', (req, res) => {
     };
 
     // TODO validate data
+    let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
     .then(doc => {
         if(doc.exists){
@@ -100,14 +101,29 @@ app.post('/signup', (req, res) => {
         }
     })
     .then(data => {
+        userId = data.user.uid;
         return auth.getIdToken(data.user);
     })
-    .then(token => {
+    .then(idToken => {
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
         return res.status(201).json({token});
     })
     .catch(err => {
         console.error(err);
-        return res.status(500).json({error: err.code});
+        if (err.code === "auth/email-already-in-use") {
+            return res.status(400).json({email: 'Email is already in use'})
+        } else {
+            return res.status(500).json({error: err.code});
+        }
     });
 });
 
