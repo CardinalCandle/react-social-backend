969f4c8ea6da80fb3ab75b28e1073392da39321b
CardinalCandle <youssef-kj@outlook.fr>
2023-08-29 23:12:02 +0100

    Added like and unlike functions An error persists : Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- functions/index.js
+++ functions/index.js
@@ -7,39 +7,122 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const functions = require('firebase-functions')
-const { Timestamp } = require('firebase-admin/firestore'); 
-const app = require('express')();
-const firebase = require('firebase/app')
-const auth = require('firebase/auth');
+const functions = require("firebase-functions");
+const { Timestamp } = require("firebase-admin/firestore");
+const app = require("express")();
+const firebase = require("firebase/app");
+const auth = require("firebase/auth");
 const { Auth } = require("firebase-admin/auth");
-require('firebase/auth');
+require("firebase/auth");
+const { db } = require("./util/admin");
 
-const { getAllPosts,postOnePost, getPost, commentOnPost, likePost, unlikePost, deletePost } = require('./handlers/posts');
-const { signup, login, uploadImage, addUserDetails, getAutheticatedUser } = require('./handlers/users');
-const FBAuth = require('./util/fbAuth')
-const firebaseConfig = require('./util/config')
-  
-  
+const {
+  getAllPosts,
+  postOnePost,
+  getPost,
+  commentOnPost,
+  likePost,
+  unlikePost,
+  deletePost,
+} = require("./handlers/posts");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAutheticatedUser,
+} = require("./handlers/users");
+const FBAuth = require("./util/fbAuth");
+const firebaseConfig = require("./util/config");
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAuth = auth.getAuth(firebaseApp);
 
 // Post routes
-app.get('/posts', getAllPosts)
-app.post('/post', FBAuth, postOnePost);
-app.get('/post/:postId', getPost);
-app.delete('/post/:postId', FBAuth, deletePost);
-app.get('/post/:postId/like', FBAuth, likePost);
-app.get('/post/:postId/unlike', FBAuth, unlikePost);
-app.post('/post/:postId/comment', FBAuth, commentOnPost);
+app.get("/posts", getAllPosts);
+app.post("/post", FBAuth, postOnePost);
+app.get("/post/:postId", getPost);
+app.delete("/post/:postId", FBAuth, deletePost);
+app.get("/post/:postId/like", FBAuth, likePost);
+app.get("/post/:postId/unlike", FBAuth, unlikePost);
+app.post("/post/:postId/comment", FBAuth, commentOnPost);
 
 // users route
-app.post('/signup', signup);
-app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage)
-app.post('/user', FBAuth, addUserDetails)
-app.get('/user', FBAuth, getAutheticatedUser)
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAutheticatedUser);
 
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region("europe-west1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("europe-west1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            reade: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions
+  .region("europe-west1")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("europe-west1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            reade: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
