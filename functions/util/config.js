02e2f5d96bee2296a43d8a395fbfbd3588b9349d
CardinalCandle <youssef-kj@outlook.fr>
2023-09-01 02:18:45 +0100

    modified gitignore file

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyDmkLnbxdpuA5AH32qJJV_MpjvxKBIMhQQ",
-    authDomain: "react-social-784b8.firebaseapp.com",
-    projectId: "react-social-784b8",
-    storageBucket: "react-social-784b8.appspot.com",
-    messagingSenderId: "1005140683266",
-    appId: "1:1005140683266:web:c452c9d6f7a44b81b3951b",
-    measurementId: "G-VM9QKFDC31"
-  };
